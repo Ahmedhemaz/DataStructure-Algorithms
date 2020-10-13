c07cd5220f7c8f30f08ad8b58a820de60fc53966
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-10-13 11:45:44 +0200

    Create BST Node innerClass

--- src/com/ahmedhemaz/DataStructrue/Trees/BST/BST.java
+++ src/com/ahmedhemaz/DataStructrue/Trees/BST/BST.java
@@ -1,4 +1,12 @@
 package com.ahmedhemaz.DataStructrue.Trees.BST;
 
 public class BST {
+    private class Node<T> {
+         T data;
+         Node<T> left, right;
+        public Node(T data){
+            this.data = data;
+            this.left = this.right = null;
+        }
+    }
 }
