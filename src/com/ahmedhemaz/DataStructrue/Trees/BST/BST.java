3884feec6cb2e409a6a10f40ffdbc2b2cc8f3da0
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-10-13 12:33:58 +0200

    Contains method to BST

--- src/com/ahmedhemaz/DataStructrue/Trees/BST/BST.java
+++ src/com/ahmedhemaz/DataStructrue/Trees/BST/BST.java
@@ -19,6 +19,24 @@ public class BST<T extends Comparable<T>> {
         }
     }
 
+    // we don't provide client to manipulate with our nodes so we provide this public method
+    // to add data to our BST
+    public void add(T data) {
+         if (data == null) throw new IllegalArgumentException();
+         if (this.root == null) {
+             this.root = new BinaryTreeNode<>(data);
+         }else{
+             this.add(data, this.root);
+         }
+         this.size++;
+    }
+
+    public boolean contains(T data) {
+        if (data == null) throw new IllegalArgumentException();
+        return contains(data, this.root);
+    }
+
+
     private void add(T data, BinaryTreeNode<T> node) {
         // if data bigger or equal to our node data then go right
         if (data.compareTo(node.data) >= 0) {
@@ -40,15 +58,20 @@ public class BST<T extends Comparable<T>> {
         }
     }
 
-    // we don't provide client to manipulate with our nodes so we provide this public method
-    // to add data to our BST
-    public void add(T data) {
-         if (data == null) throw new IllegalArgumentException();
-         if (this.root == null) {
-             this.root = new BinaryTreeNode<>(data);
-         }else{
-             this.add(data, this.root);
-         }
-         this.size++;
+    private boolean contains(T data, BinaryTreeNode<T> node) {
+
+        // if node is null then it's not found
+        if (node == null) return false;
+
+        // if data equals our node data then return true
+        if (data.compareTo(node.data) == 0) return true;
+
+        // if data bigger than node data then go right
+        if (data.compareTo(node.data) > 0){
+            return contains(data, node.right);
+        }
+
+        // if data less than node data then go left
+        return contains(data, node.left);
     }
 }
