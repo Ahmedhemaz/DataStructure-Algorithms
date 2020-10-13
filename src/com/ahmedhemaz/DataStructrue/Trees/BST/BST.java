47798fc4350dae9d5c02fb4f92b48de7c7a0df5c
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-10-13 14:40:05 +0200

    Add remove method with successor and predecessor

--- src/com/ahmedhemaz/DataStructrue/Trees/BST/BST.java
+++ src/com/ahmedhemaz/DataStructrue/Trees/BST/BST.java
@@ -36,6 +36,22 @@ public class BST<T extends Comparable<T>> {
         return contains(data, this.root);
     }
 
+    public T getSuccessor() {
+        return this.getSuccessor(root).data;
+    }
+
+    public T getPredecessor() {
+        return  this.getPredecessor(root).data;
+    }
+
+    public Integer size() {
+        return this.size;
+    }
+
+    public void remove(T data) {
+        root = remove(data, this.root);
+    }
+
 
     private void add(T data, BinaryTreeNode<T> node) {
         // if data bigger or equal to our node data then go right
@@ -73,5 +89,83 @@ public class BST<T extends Comparable<T>> {
 
         // if data less than node data then go left
         return contains(data, node.left);
+    }
+
+    private BinaryTreeNode<T> remove(T data, BinaryTreeNode<T> node) {
+
+        // if node is null then the tree is empty or there is no need to travers more
+        if (node == null) return null;
+
+        // if the value of the data being searched for is less than the value of the current root node, then
+        // traverse to the left node of the current root, setting the current left node to whatever gets returned
+        // from the delete method
+        else if (data.compareTo(node.data) < 0 ) {
+            node.left = remove(data, node.left);
+        }
+
+        // if the value of the data being searched for is greater than the value of the current root node, then
+        // traverse to the right node of the current root, setting the current right node to whatever gets returned
+        // from the delete method
+        else if (data.compareTo(node.data) > 0) {
+            node.right = remove(data, node.right);
+        }
+
+        // we found our node lets delete it
+        else {
+            if (isLeafNode(node)) {
+                return null;
+            }
+            if (isNodeWithOneChild(node)) {
+                if (node.right == null) return node.left;
+                if (node.left == null) return node.right;
+            }
+            // get the successor
+            BinaryTreeNode<T> successor = getSuccessor(node);
+            // set node data to successor data
+            node.data = successor.data;
+            // as the successor will be at the left of our node then we need to delete it
+            // as it would be with the same data as our node
+            node.left = remove(node.data, node.left);
+        }
+        return node;
+    }
+
+    private boolean isLeafNode(BinaryTreeNode<T> node) {
+        return node.left == null && node.right == null;
+    }
+    private boolean isNodeWithOneChild(BinaryTreeNode<T> node) {
+        return node.right != null || node.left != null;
+    }
+
+    private BinaryTreeNode<T> getSuccessor(BinaryTreeNode<T> node) {
+        node = node.left;
+        while (node.right != null) {
+            node = node.right;
+        }
+        return node;
+    }
+
+    private BinaryTreeNode<T> getPredecessor(BinaryTreeNode<T> node) {
+        node = node.right;
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+
+
+
+    public static void main(String[] args) {
+        BST<Integer> integerBST = new BST<>();
+        integerBST.add(10);
+        integerBST.add(15);
+        integerBST.add(6);
+        integerBST.add(4);
+        integerBST.add(8);
+        integerBST.add(12);
+        integerBST.add(20);
+        integerBST.remove(6);
+//        System.out.println(integerBST.getSuccessor());
+//        System.out.println(integerBST.getPredecessor());
     }
 }
