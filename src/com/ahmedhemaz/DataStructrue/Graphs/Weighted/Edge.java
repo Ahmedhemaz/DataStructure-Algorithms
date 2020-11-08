49e92c7ee884740b7c2e08b20893290254434330
Ahmedhemaz <ah_ibrahim94@hotmail.com>
2020-11-08 07:55:36 +0200

    Create Edge class for weighted Graphs

---
+++ src/com/ahmedhemaz/DataStructrue/Graphs/Weighted/Edge.java
@@ -0,0 +1,29 @@
+package com.ahmedhemaz.DataStructrue.Graphs.Weighted;
+
+public class Edge<T> implements Comparable<Edge<T>> {
+    private final Double weight;
+    private final T v;
+    private final T w;
+
+    public Edge(T v, T w, Double weight) {
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public T either() {
+        return this.v;
+    }
+
+    public T other(T vertex) {
+        if(vertex == this.v) return this.w;
+        return this.v;
+    }
+
+    @Override
+    public int compareTo(Edge<T> thatEdge) {
+        if (this.weight > thatEdge.weight) return 1;
+        else if (this.weight < thatEdge.weight) return -1;
+        else return 0;
+    }
+}
